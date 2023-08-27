ddfae260b86292cb0c002bd0a080bd8deeb58816
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-08-27 15:16:32 +0530

    Add files via upload

---
+++ ADA_Practicals/Practical_1/pr_3_recursion.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int sum(int n){
+
+// Base
+ if (n==1){
+    return 1;
+ }
+ // Recursive
+ else{
+    return n + sum(n - 1);
+ }
+}
+
+
+
+int main(){
+
+int N;
+printf("Yashvi Jasoliya\n");
+printf("210210116036\n");
+
+printf("Enter the N :- ");
+scanf("%d", &N);
+
+int result = sum(N);
+printf("The sum of numbers from 1 to %d :- %d", N,result);
+
+
+    return 0;
+}
