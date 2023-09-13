e5ea8012121c8947eade79418a48fde2a802ef0a
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-09-13 21:33:22 +0530

    Update pr_2_equation.c

--- ADA_Practicals/Practical_1/pr_2_equation.c
+++ ADA_Practicals/Practical_1/pr_2_equation.c
@@ -3,13 +3,13 @@
 int main(){
 
     int N, sum = 0;
-
     printf("Yashvi Jasoliya\n");
 
     printf("Enter number N :- ");
     scanf("%d", &N);
 
     sum = (N * (N + 1)) / 2;
+    
     printf("The sum of numbers from 1 to %d :- %d", N, sum);
     return 0;
 }
